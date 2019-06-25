38bba68152df91b924bb889d4a0bfb7bf386054a
Victor Pragana <victormerigue@gmail.com>
2019-06-25 14:52:30 -0300

    small changes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,43 +1,44 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
 
-const User = mongoose.model('User');
+const User = mongoose.model("User");
 
 module.exports = {
   async register(req, res) {
     const { email } = req.body;
 
     try {
-      if(await User.findOne({ email }))
-        res.status(400).send({ error: 'Usuario já existe' });
+      if (await User.findOne({ email }))
+        res.status(400).send({ error: "Usuario já existe" });
 
       const user = await User.create(req.body);
+      console.log("user", user);
 
       user.password = undefined;
 
       const token = jwt.sign({ id: user.id }, authConfig.secret, {
         expiresIn: 86400
-      });  
+      });
 
       return res.json({ user, token });
     } catch (error) {
-      res.status(400).send({ error: 'Falha ao registrar' });
+      console.log("error", error);
+      res.status(400).send({ error: "Falha ao registrar" });
     }
   },
 
   async authenticate(req, res) {
     const { email, password } = req.body;
 
-    const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select("+password");
 
-    if(!user)
-      return res.status(400).send({ error: 'Usuario não encontrado' });
+    if (!user) return res.status(400).send({ error: "Usuario não encontrado" });
 
-    if(!await bcrypt.compare(password, user.password))
-      return res.status(400).send({ error: 'Verifique o email e a senha.' });
-    
+    if (!(await bcrypt.compare(password, user.password)))
+      return res.status(400).send({ error: "Verifique o email e a senha." });
+
     user.password = undefined;
 
     const token = jwt.sign({ id: user.id }, authConfig.secret, {
@@ -46,4 +47,4 @@ module.exports = {
 
     return res.json({ user, token });
   }
-}
+};
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,8 @@ const auth = require("./middlewares/auth");
 const taskController = require("./controllers/taskController");
 const authController = require("./controllers/authController");
 
-routes.get("/tasks", auth, taskController.index);
-routes.get("/tasks/:id", auth, taskController.show);
+routes.get("/tasks", taskController.index);
+routes.get("/tasks/:id", taskController.show);
 routes.post("/tasks", auth, taskController.create);
 routes.put("/tasks/:id", auth, taskController.update);
 routes.delete("/tasks/:id", auth, taskController.destroy);
